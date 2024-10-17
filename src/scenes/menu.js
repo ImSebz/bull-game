210cdfcd52736408234447ca3ae0f75b2b5dd083
Sebastián Camargo <juansecapri@hotmail.com>
2024-10-17 10:22:14 -0500

    Funcionalidad de boton de inicio

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -9,19 +9,19 @@ export class Menu extends Phaser.Scene {
         mContext = this;
 
         startBtn.on('pointerdown', function () {
-            startBtn.setScale(.65);
+            startBtn.setScale(1.6);
             setTimeout(() => {
                 mContext.scene.start('Game');
             }, 350);
         });
 
         startBtn.on('pointerout', () => {
-            startBtn.setScale(.85);
+            startBtn.setScale(1.8);
         });
     }
 
     init() {
         this.add.image(0, 0, 'background').setOrigin(0);
-        startBtn = this.add.image(((this.game.config.width) / 2), (this.game.config.height) - 180, 'start-btn').setScale(.85).setInteractive();
+        startBtn = this.add.image(((this.game.config.width) / 2), (this.game.config.height) - 180, 'start-btn').setScale(2).setInteractive();
     }
 }
